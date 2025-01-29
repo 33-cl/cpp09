0057aba32e5a9d0aa7648f66f7c08bf31ba2989e
Mael Ferreira <maeferre@z3r6p5.42lyon.fr>
2025-01-29 18:03:47 +0100

    Adding recursivity on vector

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -9,88 +9,158 @@
     - Inserer les seconds elements
 */
 
-void    sort_vec(std::vector<int>& vec)
+std::vector<size_t> generate_jacobsthal_sequence(size_t max_size)
 {
-    if (vec.size() == 1)
-        return;
-
-    bool                is_odd_length = vec.size() % 2 != 0;
-    std::vector<int>    tmp;
-
-    for (size_t  i = 0; i < vec.size() - is_odd_length; i += 2)
-    {
-        // Sort pairs [9, 4, 7, 2, 0, 8, 2] => [4, 9], [2, 7], [0, 8], [2]
-        if (vec[i] > vec[i + 1])
-            swap(vec[i], vec[i + 1]);
-
-        // Insert first elems of pair
-        size_t pos = 0;
-        while (pos < tmp.size() && tmp[pos] < vec[i])
-            pos++;
-        tmp.insert(tmp.begin() + pos, vec[i]);
-    }
-
-    // Insert second elems of pair
-    for (size_t i = 1; i < vec.size(); i += 2)
-    {
-        size_t pos = 0;
-        while (pos < tmp.size() && tmp[pos] < vec[i])
-            pos++;
-        tmp.insert(tmp.begin() + pos, vec[i]);
-    }
-    
-    // Add the last alone event if he exists
-    if (is_odd_length)
-    {
-        size_t pos = 0;
-        while (pos < tmp.size() && tmp[pos] < vec[vec.size() - 1])
-            pos++;
-        tmp.insert(tmp.begin() + pos, vec[vec.size() - 1]);
-    }
-
-    vec = tmp;
+    std::vector<size_t> jacobsthal;
+    jacobsthal.push_back(0);
+    jacobsthal.push_back(1);
+    while (jacobsthal.back() < max_size)
+        jacobsthal.push_back(jacobsthal[jacobsthal.size() - 1] + 2 * jacobsthal[jacobsthal.size() - 2]);
+    return jacobsthal;
 }
 
-void    sort_deq(std::deque<int>& deq)
+void sort_vec(std::vector<int>& vec)
+{
+    size_t n = vec.size();
+    if (n <= 1)
+        return;
+
+    bool is_odd_length = (n % 2 != 0);
+    std::vector<int> main_chain, b_elements;
+
+    // Étape 1 : Comparer les paires et stocker les plus grands éléments pour tri
+    for (size_t i = 0; i + 1 < n; i += 2)
+    {
+        if (vec[i] > vec[i + 1])
+            std::swap(vec[i], vec[i + 1]);
+
+        main_chain.push_back(vec[i + 1]); // Stocker les plus grands pour tri
+        b_elements.push_back(vec[i]);     // Stocker les plus petits
+    }
+
+    // Ajouter l'élément impair s'il existe
+    if (is_odd_length)
+        main_chain.push_back(vec.back());
+
+    // Étape 2 : Trier récursivement les plus grands éléments (main_chain)
+    sort_vec(main_chain);
+
+    // Étape 3 : Insérer les petits éléments dans la main_chain en suivant Jacobsthal
+    std::vector<int> sorted_vec = main_chain;
+    std::vector<size_t> jacobsthal_seq = generate_jacobsthal_sequence(b_elements.size());
+
+    for (size_t i = 0; i < b_elements.size(); ++i)
+    {
+        int b = b_elements[i];
+        size_t idx = std::min(jacobsthal_seq[i], sorted_vec.size());
+        std::vector<int>::iterator pos = std::lower_bound(sorted_vec.begin(), sorted_vec.begin() + idx, b);
+        sorted_vec.insert(pos, b);
+    }
+
+    vec = sorted_vec;
+}
+
+// void sort_vec(std::vector<int>& vec)
+// {
+//     if (vec.size() == 1)
+//         return;
+
+//     bool is_odd_length = vec.size() % 2 != 0;
+//     std::vector<int> tmp;
+
+//     for (std::vector<int>::iterator it = vec.begin(); it != vec.end() - is_odd_length; it += 2)
+//     {
+//         // Sort pairs [9, 4, 7, 2, 0, 8, 2] => [4, 9], [2, 7], [0, 8], [2]
+//         if (*it > *(it + 1))
+//             std::swap(*it, *(it + 1));
+
+//         // Insert first elems of pair
+//         std::vector<int>::iterator pos = tmp.begin();
+//         while (pos != tmp.end() && *pos < *it)
+//             ++pos;
+//         tmp.insert(pos, *it);
+//     }
+
+//     // Insert second elems of pair
+//     for (std::vector<int>::iterator it = vec.begin() + 1; it < vec.end(); it += 2)
+//     {
+//         std::vector<int>::iterator pos = tmp.begin();
+//         while (pos != tmp.end() && *pos < *it)
+//             ++pos;
+//         tmp.insert(pos, *it);
+//     }
+    
+//     // Add the last alone event if he exists
+//     if (is_odd_length)
+//     {
+//         std::vector<int>::iterator pos = tmp.begin();
+//         std::vector<int>::iterator last = vec.end() - 1;
+//         while (pos != tmp.end() && *pos < *last)
+//             ++pos;
+//         tmp.insert(pos, *last);
+//     }
+
+//     vec = tmp;
+// }
+
+
+void sort_deq(std::deque<int>& deq)
 {
     if (deq.size() == 1)
         return;
 
-    bool                is_odd_length = deq.size() % 2 != 0;
-    std::deque<int>    tmp;
+    bool is_odd_length = deq.size() % 2 != 0;
+    std::deque<int> tmp;
 
-    for (size_t  i = 0; i < deq.size() - is_odd_length; i += 2)
+    for (std::deque<int>::iterator it = deq.begin(); it != deq.end() - is_odd_length; it += 2)
     {
         // Sort pairs [9, 4, 7, 2, 0, 8, 2] => [4, 9], [2, 7], [0, 8], [2]
-        if (deq[i] > deq[i + 1])
-            swap(deq[i], deq[i + 1]);
+        if (*it > *(it + 1))
+            std::swap(*it, *(it + 1));
 
         // Insert first elems of pair
-        size_t pos = 0;
-        while (pos < tmp.size() && tmp[pos] < deq[i])
-            pos++;
-        tmp.insert(tmp.begin() + pos, deq[i]);
+        std::deque<int>::iterator pos = tmp.begin();
+        while (pos != tmp.end() && *pos < *it)
+            ++pos;
+        tmp.insert(pos, *it);
     }
 
     // Insert second elems of pair
-    for (size_t i = 1; i < deq.size(); i += 2)
+    for (std::deque<int>::iterator it = deq.begin() + 1; it < deq.end(); it += 2)
     {
-        size_t pos = 0;
-        while (pos < tmp.size() && tmp[pos] < deq[i])
-            pos++;
-        tmp.insert(tmp.begin() + pos, deq[i]);
+        std::deque<int>::iterator pos = tmp.begin();
+        while (pos != tmp.end() && *pos < *it)
+            ++pos;
+        tmp.insert(pos, *it);
     }
     
     // Add the last alone event if he exists
     if (is_odd_length)
     {
-        size_t pos = 0;
-        while (pos < tmp.size() && tmp[pos] < deq[deq.size() - 1])
-            pos++;
-        tmp.insert(tmp.begin() + pos, deq[deq.size() - 1]);
+        std::deque<int>::iterator pos = tmp.begin();
+        std::deque<int>::iterator last = deq.end() - 1;
+        while (pos != tmp.end() && *pos < *last)
+            ++pos;
+        tmp.insert(pos, *last);
     }
 
     deq = tmp;
+}
+
+void verifierVecteurTrie(const std::vector<int>& vec) {
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
+        if (vec[i] > vec[i + 1]) {
+            std::cout << vec[i] << " n'est pas inférieur à " << vec[i + 1] << std::endl;
+        }
+    }
+}
+
+void verifierDequeTrie(const std::deque<int>& dq) {
+    for (size_t i = 0; i < dq.size() - 1; ++i) {
+        if (dq[i] > dq[i + 1]) {
+            std::cout << dq[i] << " n'est pas inférieur à " << dq[i + 1] << std::endl;
+        }
+    }
 }
 
 int main(int argc, char* argv[])
@@ -106,19 +176,22 @@ int main(int argc, char* argv[])
         container1 = parse_args_vec(argc, argv);
         container2 = parse_args_deq(argc, argv);
 
-        std::cout << "Before: ";
-        print_vec(container1);
+        // std::cout << "Before: ";
+        // print_vec(container1);
 
         start_vec = clock();
         sort_vec(container1);
         end_vec = clock();
 
+        //verifierVecteurTrie(container1);
+
         start_deq = clock();
         sort_deq(container2);
         end_deq = clock();
+        verifierDequeTrie(container2);
 
-        std::cout << "After: ";
-        print_vec(container1);
+        // std::cout << "After: ";
+        // print_vec(container1);
 
 
         double duration_vec = static_cast<double>(end_vec - start_vec) / CLOCKS_PER_SEC;
